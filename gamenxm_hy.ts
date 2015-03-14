c4877ac793b6a19b3739434534581dc5ed21c502
Armen Badalian <armenbadal@gmail.com>
2015-03-14 23:58:43 +0400

    Լրացումներ 6

---
+++ gamenxm_hy.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hy_AM">
+<context>
+    <name>Window</name>
+    <message>
+        <location filename="window.cpp" line="19"/>
+        <source>Game NxM</source>
+        <translation>Խաղ NxM</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="38"/>
+        <source>New</source>
+        <translation>Նոր</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="42"/>
+        <source>Exit</source>
+        <translation>Ելք</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="46"/>
+        <source>About</source>
+        <translation>Տեղեկություն</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="55"/>
+        <source>Game</source>
+        <translation>Խաղ</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="61"/>
+        <source>Help</source>
+        <translation>Հուշում</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="100"/>
+        <source>Step: </source>
+        <translation>Քայլ: </translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="102"/>
+        <source>Game N×M</source>
+        <translation>Խաղ N×M</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="102"/>
+        <source>You win at step </source>
+        <translation>Դուք հաղթեցիք, քայլ </translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="108"/>
+        <source>&lt;b&gt;Game N×M&lt;/b&gt; - extended version of &lt;i&gt;Game 15&lt;/i&gt;&lt;br/&gt;</source>
+        <translation>&lt;b&gt;Խաղ N×M&lt;/b&gt; - &lt;i&gt;Խաղ 15&lt;/i&gt;-ի ընդլայնված տարբերակը&lt;br/&gt;</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="109"/>
+        <source>Designed and implemented for educational purposes.&lt;br/&gt;&lt;br/&gt;2015</source>
+        <translation>Նախագծված և իրականացված է ուսուցողական նպատակներով:.&lt;br/&gt;&lt;br/&gt;2015</translation>
+    </message>
+</context>
+</TS>
